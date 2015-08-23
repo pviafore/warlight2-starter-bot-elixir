716ffcccf984ea5afdf65d8c9debe6b1d7c78ef8
Pat Viafore <patviafore@gmail.com>
2015-08-23 14:15:44 -0500

    Simple Game Logic will pick first starting region

--- warlight2_bot/lib/simple_game_logic.ex
+++ warlight2_bot/lib/simple_game_logic.ex
@@ -1,4 +1,17 @@
 defmodule SimpleGameLogic do
 
+  def start(sender) do
+      {:ok, logic} = Task.start_link(fn->recv(sender) end)
+      logic
+  end
+
+  def recv(sender) do
+      receive do
+         {:starting_region_choice, list} -> send sender, {:message, List.first(list)}
+         _ -> send sender, {:error, "Invalid Message Received"}
+      end
+      recv(sender)
+  end
+
 
 end
--- warlight2_bot/test/simple_game_logic_test.exs
+++ warlight2_bot/test/simple_game_logic_test.exs
@@ -1,5 +1,26 @@
 defmodule SimpleGameLogicTest do
    use ExUnit.Case
 
+   def assert_send_logic(message, expected, atom) do
+      logic = SimpleGameLogic.start self()
+      send logic, message
+      receive do
+         {a, msg} ->
+               assert msg == expected
+               assert a == atom
+         _ -> assert false, "Did not receive a well-formed message"
+      end
+   end
 
+   test "should error out on invalid message" do
+       assert_send_logic({:invalid, ""}, "Invalid Message Received", :error)
+   end
+
+   test "should pick first starting region" do
+      assert_send_logic({:starting_region_choice, [5, 7, 3, 1, 200, 12, 4]}, 5, :message)
+   end
+
+   test "should pick a different first starting region" do
+       assert_send_logic({:starting_region_choice, [7, 3, 1, 200, 12, 4]}, 7, :message)
+   end
 end
