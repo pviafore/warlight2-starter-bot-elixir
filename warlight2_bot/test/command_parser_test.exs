14f0fd461e8eb86534ad0c25f32aaaffb4bba763
Pat Viafore <patviafore@gmail.com>
2015-08-28 23:01:02 -0500

    Macro-tize command parser tests

--- warlight2_bot/test/command_parser_test.exs
+++ warlight2_bot/test/command_parser_test.exs
@@ -1,6 +1,16 @@
+defmodule CommandParserMacro do
+   defmacro test_communication(desc, msg, atom, return_msg) do
+      quote do
+         test unquote(desc) do
+             assert_command_parser_communication(unquote(msg), unquote(atom), unquote(return_msg))
+         end
+      end
+   end
+end
+
 defmodule CommandParserTest do
     use ExUnit.Case
-
+    require CommandParserMacro
     def empty_mailbox do
       receive do
          _ -> nil
@@ -19,33 +29,15 @@ defmodule CommandParserTest do
 
     end
 
-    test "sending invalid message sends out error code" do
-        assert_command_parser_communication("INVALID MESSAGE", :error, "Invalid Message Received")
-    end
+    CommandParserMacro.test_communication "sending invalid message sends out error code", "INVALID MESSAGE", :error, "Invalid Message Received"
+    CommandParserMacro.test_communication "Returns starting region list given a list",
+                                          "pick_starting_region 10000 2 6 10 19 20 26 32 33 38 45 55 62",
+                                          :starting_region_choice,
+                                           ["2", "6", "10", "19", "20", "26","32", "33", "38", "45", "55", "62"]
+    CommandParserMacro.test_communication "Returns armies to place", "go place_armies 10000", :place_armies, ""
+    CommandParserMacro.test_communication "Returns attack/transfer", "go attack/transfer 10000", :attack_transfer, ""
+    CommandParserMacro.test_communication "updates timebank", "settings timebank 1000", :initial_timebank, 1000
+    CommandParserMacro.test_communication "updates time_per_move", "settings time_per_move 500", :time_per_move, 500
+    CommandParserMacro.test_communication "updates max_round", "settings max_rounds 100", :max_rounds, 100
 
-    test "Returns starting region list given a list" do
-       assert_command_parser_communication("pick_starting_region 10000 2 6 10 19 20 26 32 33 38 45 55 62",
-                                                  :starting_region_choice,
-                                                  ["2", "6", "10", "19", "20", "26","32", "33", "38", "45", "55", "62"])
-    end
-
-    test "Returns armies to place" do
-      assert_command_parser_communication("go place_armies 10000", :place_armies, "")
-    end
-
-    test "Returns attack/transfer" do
-      assert_command_parser_communication("go attack/transfer 10000", :attack_transfer, "")
-    end
-
-    test "updates timebank" do
-      assert_command_parser_communication("settings timebank 1000", :initial_timebank, 1000)
-    end
-
-    test "updates time_per_move" do
-       assert_command_parser_communication("settings time_per_move 500", :time_per_move, 500)
-    end
-
-    test "updates max_round" do
-       assert_command_parser_communication("settings max_rounds 100", :max_rounds, 100)
-    end
 end
