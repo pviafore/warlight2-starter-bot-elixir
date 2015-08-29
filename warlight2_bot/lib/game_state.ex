93342d349211c1e163141930d75b48dd68a662b8
Pat Viafore <patviafore@gmail.com>
2015-08-28 21:33:16 -0500

    Added time per move to game state

--- warlight2_bot/lib/game_state.ex
+++ warlight2_bot/lib/game_state.ex
@@ -1,11 +1,14 @@
 defmodule GameState do
 
 def initial() do
-    %{:timebank => 0}
+    %{:timebank => 0, :time_per_move=>0}
 end
 def set_timebank(state, time) do
   %{state | :timebank => time}
 end
 
+def set_time_per_move(state, time_per_move) do
+  %{state | :time_per_move=> time_per_move}
+end
 
 end
--- warlight2_bot/test/game_state_test.exs
+++ warlight2_bot/test/game_state_test.exs
@@ -2,11 +2,15 @@ defmodule GameStateTest do
    use ExUnit.Case
 
    test "should have correct initial state" do
-       assert %{:timebank => 0} == GameState.initial()
+       assert %{:timebank => 0, :time_per_move => 0} == GameState.initial()
    end
    test "should set timebank" do
-       assert %{:timebank => 1000} == GameState.initial() |> GameState.set_timebank 1000
-       assert %{:timebank => 100} == GameState.initial |> GameState.set_timebank(1000) |> GameState.set_timebank 100
+       assert %{GameState.initial | :timebank => 1000} == GameState.initial() |> GameState.set_timebank 1000
+       assert %{GameState.initial | :timebank => 100} == GameState.initial |> GameState.set_timebank(1000) |> GameState.set_timebank 100
+   end
 
+   test "should set time per move" do
+       assert %{GameState.initial | :time_per_move=> 500} == GameState.initial() |> GameState.set_time_per_move 500
+       assert %{GameState.initial | :time_per_move=> 50} == GameState.initial() |> GameState.set_time_per_move(500) |> GameState.set_time_per_move(50)
    end
 end
