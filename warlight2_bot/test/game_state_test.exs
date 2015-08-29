c2a5741534443039f566c8621ece23fcb5131768
Pat Viafore <patviafore@gmail.com>
2015-08-28 22:45:06 -0500

    macro-tize the game state tests

--- warlight2_bot/test/game_state_test.exs
+++ warlight2_bot/test/game_state_test.exs
@@ -1,22 +1,27 @@
+defmodule GameStateTestMacro do
+
+  defmacro test_state desc, setting, func, first_val, second_val do
+
+    quote do
+       test unquote(desc) do
+          assert %{GameState.initial | unquote(setting) => unquote(first_val)} ==  apply(GameState, unquote(func), [GameState.initial(), unquote(first_val)])
+          assert %{GameState.initial | unquote(setting) => unquote(second_val)} == apply(GameState, unquote(func), [apply(GameState, unquote(func), [GameState.initial(), unquote(first_val)]), unquote(second_val)])
+
+       end
+    end
+  end
+
+end
+
 defmodule GameStateTest do
    use ExUnit.Case
-
+   require GameStateTestMacro
    test "should have correct initial state" do
        assert %{:timebank => 0, :time_per_move => 0,
                 :max_rounds => 0, :bot_name =>""} == GameState.initial()
    end
-   test "should set timebank" do
-       assert %{GameState.initial | :timebank => 1000} == GameState.initial() |> GameState.set_timebank 1000
-       assert %{GameState.initial | :timebank => 100} == GameState.initial |> GameState.set_timebank(1000) |> GameState.set_timebank 100
-   end
+   GameStateTestMacro.test_state "should set timebank", :timebank, :set_timebank, 1000, 100
+   GameStateTestMacro.test_state "should set time per move", :time_per_move, :set_time_per_move, 500, 50
+   GameStateTestMacro.test_state "should set max_rounds", :max_rounds, :set_max_rounds, 100, 200
 
-   test "should set time per move" do
-       assert %{GameState.initial | :time_per_move=> 500} == GameState.initial() |> GameState.set_time_per_move 500
-       assert %{GameState.initial | :time_per_move=> 50} == GameState.initial() |> GameState.set_time_per_move(500) |> GameState.set_time_per_move(50)
-   end
-
-   test "should set max rounds" do
-      assert %{GameState.initial | :max_rounds => 100} == GameState.initial() |> GameState.set_max_rounds 100
-      assert %{GameState.initial | :max_rounds => 200} == GameState.initial() |> GameState.set_max_rounds(100) |> GameState.set_max_rounds(200)
-   end
 end
