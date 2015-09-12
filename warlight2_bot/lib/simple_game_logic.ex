df1f7652b21ca7037f69fc81c9ad82085ccbf5f7
Pat Viafore <patviafore@gmail.com>
2015-09-12 12:19:10 -0500

    Update formatting of map access

--- warlight2_bot/lib/simple_game_logic.ex
+++ warlight2_bot/lib/simple_game_logic.ex
@@ -2,15 +2,18 @@ defmodule GameLogicMacro do
 
    defmacro create_handle_func(param_name) do
      quote do
-        defp handle(strategy, state, {unquote(:"#{param_name}"), x}) do
-           recv strategy, apply( GameState, unquote(:"set_#{param_name}"), [state, x])
+        defp handle(_strategy, state, {unquote(:"#{param_name}"), x}) do
+           apply( GameState, unquote(:"set_#{param_name}"), [state, x])
         end
      end
    end
 
    defmacro create_passalong_func(atom) do
       quote do
-        defp handle(strategy, state, {unquote(atom), _}), do: send(strategy, {unquote(atom), state})
+        defp handle(strategy, state, {unquote(atom), _})  do
+           send(strategy, {unquote(atom), state})
+           state
+        end
       end
    end
 
@@ -24,8 +27,16 @@ defmodule SimpleGameLogic do
       logic
   end
 
-  defp handle(_, state, {:state, sender}), do: send(sender, {:state, state})
-  defp handle(strategy, state, {:initial_timebank, time}), do: recv(strategy, GameState.set_timebank(state, time))
+  defp handle(_, state, {:state, sender}), do
+     send(sender, {:state, state})
+     state
+  end
+  
+  defp handle(strategy, state, {:initial_timebank, time}) do
+    recv(strategy, GameState.set_timebank(state, time))
+    state
+  end
+
   GameLogicMacro.create_handle_func "time_per_move"
   GameLogicMacro.create_handle_func "max_rounds"
   GameLogicMacro.create_handle_func "bot_name"
@@ -41,13 +52,22 @@ defmodule SimpleGameLogic do
   GameLogicMacro.create_handle_func "last_opponent_moves"
   GameLogicMacro.create_passalong_func :place_armies
   GameLogicMacro.create_passalong_func :attack_transfer
-  defp handle(strategy, state, {:starting_region_choice, list}), do: send(strategy, {:pick_starting, {list,state}})
-  defp handle(strategy, state, {:update_map, regions}), do: recv(strategy, GameState.update_map(state, regions))
-  defp handle(strategy, _, _), do: send( strategy, {:error, "Invalid Message Received"})
+
+  defp handle(strategy, state, {:starting_region_choice, list}) do
+      send(strategy, {:pick_starting, {list,state}})
+      state
+  end
+
+  defp handle(strategy, state, {:update_map, regions}), do: GameState.update_map(state, regions)
+
+  defp handle(strategy, _, _) do
+    send( strategy, {:error, "Invalid Message Received"})
+    state
+  end
 
   def recv(strategy, state) do
       receive do
-         m -> handle strategy, state, m
+         m -> recv(strategy, handle (strategy, state, m)
       end
       recv(strategy,  state)
   end
