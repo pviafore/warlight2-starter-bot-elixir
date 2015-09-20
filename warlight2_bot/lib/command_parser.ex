b7417bc4c8a246dd9e62179194c2d6bca754fa9f
Pat Viafore <patviafore@gmail.com>
2015-09-19 21:27:20 -0500

    Starting work on deconstructing command parser rather than using a cond statement

--- warlight2_bot/lib/command_parser.ex
+++ warlight2_bot/lib/command_parser.ex
@@ -24,10 +24,27 @@ defmodule CommandParser do
      send engine, {atom, String.split(List.last(matches))}
   end
 
+  def handle(game_engine, _, ["pick_starting_region", _time | regions] ), do: send(game_engine, {:starting_region_choice, regions})
+  def handle(game_engine, _, ["go", "attack/transfer" | _]), do: send(game_engine, {:attack_transfer, ""})
+  def handle(game_engine, _, ["go", "place_armies" | _]), do: send(game_engine, {:place_armies, ""})
+  def handle(game_engine, _, ["opponent_moves"  | msg]) do
+     moves = msg |> Enum.chunk(5) |> Enum.map(fn [a,b,c,d,e] -> {a,b, c, d, String.to_integer e} end)
+     send game_engine, {:last_opponent_moves, moves}
+  end
+  def handle(game_engine, _, ["update_map" | msg]) do
+    send game_engine, {:update_map, msg |> Enum.chunk(3) |> Enum.map(fn [a, b, c] -> {a, b, String.to_integer c} end)}
+  end
+
+  def handle(game_engine, logger, msg) do
+      CustomLogger.write(logger, "Command Parser didn't know how to parse message: " <> Enum.join msg, ",")
+      send game_engine, {:error, "Invalid Message Received"}
+  end
+
   def parse(game_engine, logger) do
      receive do
         {:message, msg} ->
             CustomLogger.write(logger, msg)
+            split_msg = String.split msg
             cond do
             Regex.match?(~r/settings timebank (\d+)/, msg) ->
                  send_int game_engine, ~r/settings timebank (\d+)/, msg, :initial_timebank
@@ -60,24 +77,8 @@ defmodule CommandParser do
                   send_list game_engine, ~r/setup_map wastelands ((?:\d+\s*)+)/, msg, :wastelands
             Regex.match?(~r/setup_map opponent_starting_regions ((?:\d+\s*)+)/, msg) ->
                   send_list game_engine, ~r/setup_map opponent_starting_regions ((?:\d+\s*)+)/, msg, :opponent_starting_regions
-            Regex.match?(~r/update_map ((?:\d+ \w+ \d+\s*)+)/, msg) ->
-                  matches = Regex.run(~r/update_map ((?:\d+ \w+ \d+\s*)+)/, msg)
-                  send game_engine, {:update_map, matches |> List.last |> String.split |> Enum.chunk(3) |> Enum.map(fn [a, b, c] -> {a, b, String.to_integer c} end)}
-            Regex.match?(~r/opponent_moves ((?:\w+ (?:place_armies|attack\/transfer) \d+ \d+ \d+\s*)+)/, msg) ->
-                  matches = Regex.run(~r/opponent_moves ((?:\w+ (?:place_armies|attack\/transfer) \d+ \d+ \d+\s*)+)/, msg)
-                  moves = matches |> List.last |>String.split |> Enum.chunk(5) |> Enum.map(fn [a,b,c,d,e] -> {a,b, c, d, String.to_integer e} end)
-                  send game_engine, {:last_opponent_moves, moves}
-            Regex.match?(~r/go place_armies \d+/, msg) ->
-                send game_engine, {:place_armies, ""}
-            Regex.match?(~r/go attack\/transfer \d+/, msg) ->
-                send game_engine, {:attack_transfer, ""}
-            Regex.match?(~r/pick_starting_region \d+ ((?:\d+ )+)/, msg) ->
-                send_list game_engine, ~r/pick_starting_region \d+ ((?:\d+\s*)+)/, msg, :starting_region_choice
-            Regex.match?(~r/opponent_moves/, msg) -> nil
-            Regex.match?(~r/update_map/, msg) -> nil
             true->
-              CustomLogger.write(logger, "Command Parser didn't know how to parse message: " <> msg)
-              send game_engine, {:error, "Invalid Message Received"}
+              handle game_engine, logger, split_msg
             end
         _ -> CustomLogger.write(logger, "Command Parser received invalid command ")
              send game_engine, {:error, "Invalid Message Received"}
